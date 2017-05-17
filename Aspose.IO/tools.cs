3ed2d5030cc591b788388de086e0619a5bd88d2f
Ed <yinghua.lin@ischool.com.tw>
2017-05-17 16:11:43 +0800

    2017/5/17 穎驊新增 此 Library ，用途為 界接 新舊版本 Aspose 格式差異問題。

---
+++ Aspose.IO/tools.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Words;
+using System.IO;
+
+namespace Aspose.IO
+{
+    public class tools
+    {
+
+        // 2017/5/17 穎驊新增此Library 專案，用意為界接 新舊 Aspose 版本功能差異的問題
+        // SavePDFtoStream  可以使用新的 Aspose 功能 解決 舊版 Aspose 無法儲存 PDF 格式 的缺點
+
+        public static MemoryStream SavePDFtoStream(MemoryStream stream)
+        {
+            Document doc = new Document(stream);
+
+            MemoryStream stream_pdf = new MemoryStream();
+
+            doc.Save(stream_pdf, SaveFormat.Pdf);
+
+            return stream_pdf;
+        }
+
+        public static void  SavePDFtoLocal(MemoryStream stream, string fileName)
+        {
+            Document doc = new Document(stream);
+
+            doc.Save(fileName, SaveFormat.Pdf);            
+        }
+    }
+}
